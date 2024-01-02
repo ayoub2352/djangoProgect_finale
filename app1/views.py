13695d67a65efa9c79b0a78cd3663a517f2f13c0
ayoub2352 <boulmeghrasayoub@gmail.com>
2024-01-02 15:57:32 +0100

    templates + urls

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request) : 
+
+  context = {}
+  return render(request,"app1/home.html" , context)
+
+def test2(request) : 
+
+  context = {}
+  return render(request,"app1/test2.html" , context)
