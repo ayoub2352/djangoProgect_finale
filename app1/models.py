7250b97ccc99300fe3b05585551e2e628d5efc43
CallMeAkram <akramelmouden15@gmail.com>
2024-01-02 13:18:21 +0100

    Created Clent_voyage relation

--- app1/models.py
+++ app1/models.py
@@ -19,13 +19,22 @@ class Client(models.Model):
     adresse = models.CharField(max_length=100)
     CIN = models.CharField(max_length=8)
     solde = models.IntegerField(default=0)
+    # client_image = models.ImageField(upload_to='images/')
 
-class voyage(models.Model):
+class Voyage(models.Model):
     titre = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(max_length=1000)
     date_depart = models.DateField()
-    date_arrivee = models.DateField()
-    prix = models.IntegerField()
-    nbr_places = models.IntegerField()
-    # image = models.ImageField(upload_to='images/')
-    
+    date_arrivee = models.DateField() 
+    prix = models.IntegerField(null=False, blank=False)#🚩here can be blank because it's only admin tasks , it can be that the admin dont know yet the price of the voyage
+    nbr_places = models.IntegerField(validators=[
+        MaxValueValidator(10),
+        MinValueValidator(10)
+        ]
+    )
+    # image_voyage = models.ImageField(upload_to='images/')
+
+class Client_voyage(models.Model):
+    fk_client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    fk_voyage = models.ForeignKey(Voyage, on_delete=models.CASCADE)
+    date_reservation = models.DateField()
